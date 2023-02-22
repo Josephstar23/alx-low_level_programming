04230d8b481423591da7f38679edb91df4f96f8b
Omovie Akpesiri <omovieakpesiri@gmail.com>
2023-02-22 05:07:02 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,22 +7,22 @@
 
 void jack_bauer(void)
 {
-	int hour, minute;
+	int i, j;
 
-	hour = 0;
-	while (hour < 24)
+	i = 0;
+	while (i < 24)
 	{
-		minute = 0;
-		while (minute < 60)
+		j = 0;
+		while (j < 60)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minte % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			minute++;
+			j++;
 		}
-		hour++
+		i++;
 	}
 }
