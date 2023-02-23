0eab71a4b027f7b20dbd39bda76e04251af67b49
Omovie Akpesiri <omovieakpesiri@gmail.com>
2023-02-23 15:59:42 -0800

    Speaking numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for and prints the numbers (0 through 9)
+ *
+ * Return: Always 0
+ */
+
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
