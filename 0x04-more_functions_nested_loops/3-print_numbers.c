c79f1fb8a55c5378c3268387fbae816fcfe52fa1
Omovie Akpesiri <omovieakpesiri@gmail.com>
2023-02-23 16:18:15 -0800

    Speaking numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,13 @@
  * Return: Always 0
  */
 
-int print_numbers(void)
+void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = 0; i < 9 ; i++)
+	for (c = 0; c < 9 ; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
