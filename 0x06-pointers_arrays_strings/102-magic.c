df37fbda3b09e86fd032554221f2e5354685f183
Omovie Akpesiri <omovieakpesiri@gmail.com>
2023-03-03 08:28:16 -0800

    Adding a new line of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints the new line of code
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -12,9 +15,6 @@ int main(void)
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
