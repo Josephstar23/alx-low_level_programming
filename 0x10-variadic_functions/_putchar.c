07549b9adeb33d79d3adfb3d239a1ff9940c0f81
Omovie Akpesiri <omovieakpesiri@gmail.com>
2023-03-24 21:56:57 -0700

    Returning the sum of parameters

---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to be written
+ * return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
