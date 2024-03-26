84b74c7e9eedf44b1462765dc13b6626524d86f7
Josephstar23 <omovieakpesiri@gmail.com>
2024-03-25 22:04:28 -0700

    Dynamic containing callable C functions from Python

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of subtaction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b:  second integer
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of modulus
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
